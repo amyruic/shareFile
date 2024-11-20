ce64b93225cd271a97d2f8f379e7404bfb71237a
amyrd <kuyaamyr@gmail.com>
2024-11-19 23:48:07 -0600

    Sync changes

--- sync_files.sh
+++ sync_files.sh
@@ -4,4 +4,4 @@ git add .
 git commit -m "Sync changes"
 git push
 
-ssh -p 8076 ldayr2@bits-head.cs.uic.edu "cd shareFile/ && git pull"
+# ssh -p 8076 ldayr2@bits-head.cs.uic.edu "cd shareFile/ && git pull"
